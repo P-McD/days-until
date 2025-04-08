476668160203670dde48983141095d7633b4d120
Poppy M <118982217+P-McD@users.noreply.github.com>
2025-04-08 14:19:09 +0100

    refactor: refactor logic to display the remaining days into seperate function

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import calculateDaysUntilDate from "./calculateDaysUntilDate.js";
 
+function displayDaysRemaining (elementId, daysRemaining){
+    const daysElement = document.getElementById(elementId);
+    daysElement.innerText = daysRemaining;
+}
 function calculateDatesAndDisplay() {
   const currentDate = new Date();
   const christmasDate = new Date("2025-12-25");
   const newYearsDate = new Date("2026-01-01");
 
   const daysUntilChristmas = calculateDaysUntilDate(currentDate, christmasDate);
-  const christmasDaysElement = document.getElementById("days-until-christmas");
-  christmasDaysElement.innerText = daysUntilChristmas;
+  displayDaysRemaining("days-until-christmas", daysUntilChristmas);
 
   const daysUntilNewYear = calculateDaysUntilDate(currentDate, newYearsDate);
-  const newYearsDaysElement = document.getElementById("days-until-new-year");
-  newYearsDaysElement.innerText = daysUntilNewYear;
+  displayDaysRemaining("days-until-new-year", daysUntilNewYear)
 }
 
 calculateDatesAndDisplay();
+
