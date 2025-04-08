96d58cab9d5a7a7ece600a48642a56da65f042c6
Poppy M <118982217+P-McD@users.noreply.github.com>
2025-04-08 14:05:04 +0100

    refactor: refactor index.js to use calculateDaysUntilDate function for clarity

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-function f1() {
-    var p = new Date();
-    var q = new Date("2025-12-25");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element");
-    variable2.innerText = variable;
-  }
-  
-  function f2() {
-    var p = new Date();
-    var q = new Date("2026-01-01");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element2");
-    variable2.innerText = variable;
-  }
-  
-  f1();
-  f2();
+import calculateDaysUntilDate from "./calculateDaysUntilDate.js";
+
+function calculateDatesAndDisplay() {
+  const currentDate = new Date();
+  const christmasDate = new Date("2025-12-25");
+  const newYearsDate = new Date("2026-01-01");
+
+  const daysUntilChristmas = calculateDaysUntilDate(currentDate, christmasDate);
+  const christmasElement = document.getElementById("my-element");
+  christmasElement.innerText = daysUntilChristmas;
+
+  const daysUntilNewYear = calculateDaysUntilDate(currentDate, newYearsDate);
+  const newYearsElement = document.getElementById("my-element");
+  newYearsElement.innerText = daysUntilNewYear;
+}
+
+calculateDatesAndDisplay();
