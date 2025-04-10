080101e51805b56ac7e16b04d2c08b32bef59482
Poppy M <118982217+P-McD@users.noreply.github.com>
2025-04-10 21:53:53 +0100

    feat: migrate project to react

---
+++ src/components/CountdownCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CountdownCard({eventName, daysRemaining, imageUrl}) {
+  return (
+    <div><h3>Days Until {eventName}</h3></div>
+  )
+}
+
+export default CountdownCard
